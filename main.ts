3178d85e808b45db13d24376cd6ab86d63cc9303
SachaNevsky <sacha.nevsky@gmail.com>
2025-12-09 11:54:21 +0100

    fixes 6

--- main.ts
+++ main.ts
@@ -8,8 +8,8 @@ const newInput = document.getElementById("newInput") as HTMLTextAreaElement;
 const diffBtn = document.getElementById("diffBtn") as HTMLButtonElement;
 
 const runner = new WebPerlRunner({
-    webperlBasePath: "./vendor/wasm-latex-tools/core/webperl",
-    perlScriptsPath: "./vendor/wasm-latex-tools/core/perl",
+    webperlBasePath: "./vendor/wasm-latex-tools/webperl",
+    perlScriptsPath: "./vendor/wasm-latex-tools/perl",
 });
 
 function setStatus(msg: string) {
