487b87df025d7392887e7feddf8d6f48a05db9ab
SachaNevsky <sacha.nevsky@gmail.com>
2025-12-12 17:16:28 +0100

    fixes 41

--- vendor/swiftlatex/swiftlatexpdftex.js
+++ vendor/swiftlatex/swiftlatexpdftex.js
@@ -5,7 +5,8 @@ var Module = {};
 self.memlog = "";
 self.initmem = undefined;
 self.mainfile = "main.tex";
-self.texlive_endpoint = "https://texlive2.swiftlatex.com/";
+// self.texlive_endpoint = "https://texlive2.swiftlatex.com/";
+self.texlive_endpoint = "./vendor/swiftlatex/";
 Module["print"] = function (a) {
   self.memlog += a + "\n";
 };
